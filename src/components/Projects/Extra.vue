7fdfa612f05ecb9102ed84653574f5b9c38f425a
SumedhaSinghRathor <sumedharathor@gmail.com>
2026-05-08 23:50:16 +0530

    restore original code

--- src/components/Projects/Extra.vue
+++ src/components/Projects/Extra.vue
@@ -3,45 +3,44 @@
     v-if="selectExtra"
     class="fixed inset-0 bg-black/30"
     @click="$emit('close')"
+  ></div>
+  <div
+    :class="[
+      'fixed top-0 right-0 bg-gray-300 flex flex-col justify-between h-full w-md transition-transform duration-300 rounded-l-lg p-2',
+      selectExtra ? 'translate-x-0' : 'translate-x-full',
+    ]"
   >
-    <div
-      :class="[
-        'fixed top-0 right-0 bg-gray-300 flex flex-col justify-between h-full w-md transition-transform duration-300 rounded-l-lg p-2',
-        selectExtra ? 'translate-x-0' : 'translate-x-full',
-      ]"
-    >
-      <div class="flex items-center gap-4">
-        <img :src="project.logo" :alt="project.title" class="size-9" />
-        <h1 class="text-3xl font-bold">{{ project.title }}</h1>
-      </div>
-      <p class="text-sm">{{ project.desc }}</p>
-      <ImgSet v-if="project.carousel" :imgset="project.carousel" />
-      <div>
-        <h2 class="font-semibold underline mb-1">
-          Information about the project
-        </h2>
-        <div class="flex flex-col gap-1 self-end">
+    <div class="flex items-center gap-4">
+      <img :src="project.logo" :alt="project.title" class="size-9" />
+      <h1 class="text-3xl font-bold">{{ project.title }}</h1>
+    </div>
+    <p class="text-sm">{{ project.desc }}</p>
+    <ImgSet v-if="project.carousel" :imgset="project.carousel" />
+    <div>
+      <h2 class="font-semibold underline mb-1">
+        Information about the project
+      </h2>
+      <div class="flex flex-col gap-1 self-end">
+        <div
+          v-for="(value, key) in filteredKeys"
+          :key="key"
+          class="flex items-center gap-2 flex-wrap"
+        >
+          <span class="font-semibold capitalize text-sm">{{ key }}: </span>
           <div
-            v-for="(value, key) in filteredKeys"
-            :key="key"
-            class="flex items-center gap-2 flex-wrap"
+            v-for="(item, i) in value"
+            :key="i"
+            class="text-xs bg-cyan-100 px-2 py-0.5 text-blue-500 rounded-full font-semibold hover:text-white hover:bg-blue-500 cursor-pointer"
           >
-            <span class="font-semibold capitalize text-sm">{{ key }}: </span>
-            <div
-              v-for="(item, i) in value"
-              :key="i"
-              class="text-xs bg-cyan-100 px-2 py-0.5 text-blue-500 rounded-full font-semibold hover:text-white hover:bg-blue-500 cursor-pointer"
-            >
-              {{ item }}
-            </div>
+            {{ item }}
           </div>
         </div>
       </div>
-      <i
-        class="bx bx-x absolute top-2 right-2 text-3xl cursor-pointer"
-        @click="$emit('close')"
-      ></i>
     </div>
+    <i
+      class="bx bx-x absolute top-2 right-2 text-3xl cursor-pointer"
+      @click="$emit('close')"
+    ></i>
   </div>
 </template>
 
